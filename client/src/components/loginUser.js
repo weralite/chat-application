d325ce8fa72af2c12ce20ed780552d6fc8d1f656
mazmann <sadi.lundberg@hotmail.com>
2024-04-05 10:46:53 +0200

    added navigation from register to login

--- client/src/components/loginUser.js
+++ client/src/components/loginUser.js
@@ -22,15 +22,14 @@ const LoginUser = () => {
         try {
             const response = await axios.post('http://localhost:8080/api/v1/auth/login', { username, password });
             const { token, user } = response.data;
-            console.log('Storing token:', token);
+
             localStorage.setItem('token', token); // Store token in localStorage
             localStorage.setItem('username', username); // Store username in localStorage
             localStorage.setItem('userId', user._id); // Store user ID in localStorage
-            console.log('Login successful:', response.data);
+
             // Redirect to the chat page
             navigate('/chat');
-            // setToken(token); // Update token in context
-            // Optionally, you can perform additional actions after successful login, such as redirecting the user to another page
+            
         } catch (error) {
             console.log('Login failed:', error.response.data.message);
             setError(error.response.data.message);
--- client/src/components/registerUser.js
+++ client/src/components/registerUser.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const RegisterUser = () => {
     const [username, setUsername] = useState('');
@@ -20,6 +21,7 @@ const RegisterUser = () => {
             console.log('Registration successful:', response.data);
             setUsername('');
             setPassword('');
+            navigate('/');
         } catch (error) {
             console.log('Registration failed');
         }
