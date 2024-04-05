36e6990367aa9e7cd6c45027ce23b0b67c2a5f62
mazmann <sadi.lundberg@hotmail.com>
2024-04-05 13:38:41 +0200

    merged

--- client/src/components/chat/chat.js
+++ client/src/components/chat/chat.js
@@ -30,7 +30,7 @@ const Chat = () => {
 
     const fetchContacts = async () => {
         try {
-            const response = await axios.get(`http://localhost:8080/api/v1/contacts/getContactsForUser`, {
+            const response = await axios.get(`http://localhost:8080/api/v1/contacts/getContacts`, {
                 params: {
                     userId: userId
                 }
