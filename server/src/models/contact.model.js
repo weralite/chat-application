178b9aee0831cdb9f07330a1ecf07c4374cd7028
mazmann <sadi.lundberg@hotmail.com>
2024-04-06 11:11:16 +0200

    updated contact schema with booleon value for being blocked or not

--- server/src/models/contact.model.js
+++ server/src/models/contact.model.js
@@ -4,7 +4,8 @@ const contactSchema = new mongoose.Schema(
   {
     user1Id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     user2Id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    blocked: { type: Boolean, default: false },
+    blockedByUser1: { type: Boolean, default: false }, // Indicates if user1 has blocked user2
+    blockedByUser2: { type: Boolean, default: false }, // Indicates if user2 has blocked user1
   },
   {
     timestamps: true,
