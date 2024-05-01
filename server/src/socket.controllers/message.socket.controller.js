405ee9fd56dfe7a5fefcafb34e56e42ffee01738
mazmann <sadi.lundberg@hotmail.com>
2024-05-01 17:44:24 +0200

    reduced code for sending and receving msgs

--- server/src/socket.controllers/message.socket.controller.js
+++ server/src/socket.controllers/message.socket.controller.js
@@ -1,7 +1,7 @@
 const Message = require('../models/message.model');
 
 
-module.exports = (io) => {
+module.exports = (io, emitToUser) => {
     io.on('connection', (socket) => {
         socket.on('get_messages', async ({ chatId }) => {
             try {
@@ -22,9 +22,20 @@ module.exports = (io) => {
              
                 message.status = 'delivered';
                 await message.save();
-                socket.emit('message_status_updated', message);
-                socket.broadcast.emit('message_status_updated', message);
-                socket.broadcast.emit('receive_messages', [message]);
+                
+                const users = [sender, receiver];
+
+                const events = [
+                    { eventName: 'receive_messages', eventData: [message] },
+                    { eventName: 'message_status_updated', eventData: message },
+                ];
+
+                users.forEach(user => {
+                    events.forEach(event => {
+                        emitToUser(user, event.eventName, event.eventData);
+                    });
+                });
+
             } catch (error) {
                 console.error('Error sending message:', error);
                 socket.emit('error', 'Failed to send message');
@@ -37,6 +48,13 @@ module.exports = (io) => {
                 message.status = 'read';
                 await message.save();
                 socket.emit('message_status_updated', message);
+
+//// Convert the following code to use emitToUser, similar to the code above.
+
+                //// Ids might have to be converted into strings?
+
+                console.log('receiver', message.receiver);
+                console.log('sender', message.sender);
                 socket.broadcast.emit('message_status_updated', message);
             } catch (error) {
                 console.error('Error updating message status:', error);
