e1d77244ad5e2bd6247634b1d64530bf14e19e61
mazmann <sadi.lundberg@hotmail.com>
2024-05-01 12:43:03 +0200

    added forgotten emit

--- client/src/components/contacts/contacts.js
+++ client/src/components/contacts/contacts.js
@@ -165,7 +165,8 @@ const Contacts = ({ userId, contacts, setContacts, setActiveChat, chatList, setC
                                                 :
                                                 <button onClick={() => blockContact(user._id)}>Block</button>
                                             }
-                                            <button onClick={() => deleteContact(user._id)}>Delete</button>
+                                            <button onClick={() => deleteContact(user._id)}>Delete chat</button>
+                                            <button onClick={() => deleteContact(user._id)}>Delete contact</button>
                                         </div>
                                     )}
                                 </li>
--- server/src/socket.controllers/contact.socket.controller.js
+++ server/src/socket.controllers/contact.socket.controller.js
@@ -18,6 +18,7 @@ module.exports = (io, emitToUser) => {
 
                 emitToUser(blockedUserId, 'contactBlocked', { contactId, blockedBy: userId });
                 emitToUser(blockedUserId, 'requestChatUpdate', { userId, blockedUserId });
+                emitToUser(blockedUserId, 'blockContactSuccess', { contactId, blockedBy: userId });
                 emitToUser(userId, 'requestChatUpdate', { userId, blockedUserId });
                 emitToUser(userId, 'blockContactSuccess', { contactId, blockedBy: userId });
 
@@ -42,6 +43,7 @@ module.exports = (io, emitToUser) => {
 
                 emitToUser(unBlockedUserId, 'contactUnblocked', { contactId, unblockedBy: userId });
                 emitToUser(unBlockedUserId, 'requestChatUpdate', { userId, blockedUserId: contactId });
+                emitToUser(unBlockedUserId, 'unblockContactSuccess', { contactId, unblockedBy: userId });
                 emitToUser(userId, 'requestChatUpdate', { userId, blockedUserId: contactId });
                 emitToUser(userId, 'unblockContactSuccess', { contactId, unblockedBy: userId });
 
